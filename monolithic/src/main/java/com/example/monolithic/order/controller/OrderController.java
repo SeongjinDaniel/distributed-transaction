f926c8ef34e37431dda6f833034833165fd3048c
seongjinOliver <yoosngj@gmail.com>
2025-09-18 07:59:44 +0900

    add order lock

--- monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
+++ monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
@@ -5,18 +5,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.monolithic.order.application.OrderService;
+import com.example.monolithic.order.application.RedisLockService;
 import com.example.monolithic.order.application.dto.CreateOrderResult;
 import com.example.monolithic.order.controller.dto.CreateOrderRequest;
 import com.example.monolithic.order.controller.dto.CreateOrderResponse;
 import com.example.monolithic.order.controller.dto.PlaceOrderRequest;
+import com.example.monolithic.order.infrastructure.OrderRepository;
 
 @RestController
 public class OrderController {
 
 	private final OrderService orderService;
 
-	public OrderController(OrderService orderService) {
+	private final RedisLockService redisLockService;
+
+	public OrderController(
+		OrderService orderService,
+		RedisLockService redisLockService
+	) {
 		this.orderService = orderService;
+	  this.redisLockService = redisLockService;
 	}
 
 	@PostMapping("/order")
@@ -30,6 +38,17 @@ public class OrderController {
 	public void placeOrder(
 		@RequestBody PlaceOrderRequest request
 	) throws InterruptedException {
+	  String key = "order:monolithic:" + request.orderId();
+	  boolean acquiredLock = redisLockService.tryLock(key, request.orderId().toString());
+
+	  if (!acquiredLock) {
+		throw new IllegalStateException("락획득에 실패하였습니다.");
+	  }
+
+	  try {
 		orderService.placeOrder(request.toPlaceOrderCommand());
+	  } finally {
+		redisLockService.releaseLock(key);
+	  }
 	}
 }
